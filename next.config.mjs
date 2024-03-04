ac2595d9b9aa1bc1e982f00c66e4403b501e5485
shanmugam_manikandan <shanmugam.manikandan@cognizant.com>
2024-03-04 22:07:42 +0800

    git action with page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/tallyCounter",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
